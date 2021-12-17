381aac84fd2faa05d9f653c69048b08e2de47630
sinceseptember4 <sinceseptember4@gmail.com>
2021-12-17 18:05:03 +0900

    jsxisok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,10 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+ReactRailsUJS.useContext(componentRequireContext);// Support component names relative to this directory:
+
